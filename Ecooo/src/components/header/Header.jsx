b2b71cf41c01cef6f60504843eb969dcdbe0bcf1
aromal0101 <aromalpradeep117@gmail.com>
2024-09-27 22:51:26 +0530

    space added

--- Ecooo/src/components/header/Header.jsx
+++ Ecooo/src/components/header/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
             <h2>
                 NO more animals are going to extinct
             </h2>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem corrupti facere nisi rerum eaque, sint molestias magnam quaerat aliquid eius illo ea odio adipisci quasi rem at? Excepturi, odit dolor.</p>
+            <p>Lorem  ipsum dolor sit, amet consectetur adipisicing elit. Autem corrupti facere nisi rerum eaque, sint molestias magnam quaerat aliquid eius illo ea odio adipisci quasi rem at? Excepturi, odit dolor.</p>
             <button>explore</button>
         </div>
 
